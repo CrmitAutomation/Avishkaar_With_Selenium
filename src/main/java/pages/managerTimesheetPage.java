8370b978b036d7adec613e569a6994890544fa64
CrmitAutomation <automationcrmitworkspace@gmail.com>
2025-06-20 20:17:57 +0530

    Timesheet Script updated

--- src/main/java/pages/managerTimesheetPage.java
+++ src/main/java/pages/managerTimesheetPage.java
@@ -12,7 +12,7 @@ public class managerTimesheetPage {
 
     // Locators
     private By engagementTab = By.xpath("//div[@class='themeHeaderBottomRow']//ul//li[4]");
-    private By timesheetsApprovalTab = By.xpath("//h3[@data-target='data-target='timesheet-approval/Timesheet_Approval__c/00B1K00000AN3gwUAD']");
+    private By timesheetsApprovalTab = By.xpath("//h3[@data-target='timesheet-approval/Timesheet_Approval__c/00B1K00000AN3gwUAD']");
     private By listViewButton = By.xpath("//button[@data-value='Pending for Approval']");
     private By pendingForApprovalDropDown = By.xpath("//lightning-base-combobox-item[@data-value='Pending_for_Approval']");
     private By timesheetApprovalLink = By.xpath("//tbody[@class='slds-scrollable_y']//a");
@@ -43,7 +43,6 @@ public class managerTimesheetPage {
 
     public String getTimesheetApprovalLink() throws InterruptedException {
         recordName = driver.findElement(timesheetApprovalLink).getText();
-        driver.findElement(timesheetApprovalLink).click(); // open the record
         Thread.sleep(2000);
         return recordName;
     }
